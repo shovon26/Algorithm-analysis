bc3b6014b51599483611a17d0a736c9edd43eb7f
shovon26 <36441192+shovon26@users.noreply.github.com>
2018-11-01 16:52:39 +0600

    Add files via upload

--- Number of connected components.cpp
+++ Number of connected components.cpp
@@ -2,39 +2,53 @@
 using namespace std;
 vector<int>adj[100];
 bool vis[100];
+int cost[100];
 void dfs(int s)
 {
     vis[s]=1;
-    for(int i=0;i<adj[s].size();i++)
+    for(int i=0; i<adj[s].size(); i++)
     {
         int k=adj[s][i];
         if(vis[k]==0)
         {
+            cost[k]=cost[s]+1;
             dfs(k);
         }
     }
 }
 int main()
 {
-    int n,e,u,v,i,j;
-    cin>>n>>e;
-    for(i=0;i<n;i++)
+    int n,e,u,v,i,j,s,m;
+    cin>>n>>m;
+    for(i=0; i<m; i++)
     {
         cin>>u>>v;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    for(i=0;i<n;i++)vis[i]=0;
+    for(i=0; i<n; i++)
+        vis[i]=0;
     int cnt=0;
-    for(i=1;i<=n;i++)
+    for(i=1; i<=n; i++)
     {
         if(vis[i]==0)
         {
+            cout<<vis[i]<<" ";
             dfs(i);
             cnt++;
         }
     }
-    cout<<cnt<<endl;
+    cout<<endl;
 
-        return 0;
+    cout<<"Number of connected components :: "<<cnt<<endl;
+    cout<<"Source Node :: ";
+    cin>>s;
+    dfs(s);
+    cout<<"Cost to visit from the source node"<<endl;
+    for(i=1; i<=n; i++)
+    {
+        cout<<i<<" "<<cost[i]<<" "<<endl;
+    }
+
+    return 0;
 }
